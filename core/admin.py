0cf8f73a0960fc3ed55025ddf2fceca91802efc2
Leandro Damasio <ldamasio@gmail.com>
2024-08-12 01:55:00 -0300

    feat: Customize User on Admin area

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import CustomUser
 
 class CustomModelAdmin(admin.ModelAdmin):
-  list_display = ["nome", "email"]
+  list_display = ["name", "email"]
   readonly_fields = ["email"]
 
 admin.site.register(CustomUser, CustomModelAdmin)
